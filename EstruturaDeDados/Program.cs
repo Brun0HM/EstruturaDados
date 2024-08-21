693cb2721fe0832568b5b780cbca7a636c784fba
Brun0HM <brunodev41@gmail.com>
2024-08-21 14:54:09 -0300

    LIsta de frutas

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -1,14 +1,29 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("My first branch");
-        Console.WriteLine("Working witht Git\\Github");
-        Console.WriteLine("ablubleblelblelbelbleblelbe");
-        Console.WriteLine("ablubleblelblelbelbleblelbe");
-        Console.WriteLine("ablubleblelblelbelbleblelbe");
+        List<string> frutas = new List<string> { "Morango", "Manga", "Goiaba", "Uva" };
 
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
 
+        Console.WriteLine("\nFruta no índice 2: " + frutas[2]);
 
+        frutas.Insert(1, "Maracujá");
+        frutas[4] = "Pêra";
+
+        Console.WriteLine("\nLista Atualizada:");
+        frutas.ForEach(Console.WriteLine);
+
+        frutas.RemoveAt(3);
+        frutas.Remove("Morango");
+
+        Console.WriteLine("\nLista Após Remoções:");
+        frutas.ForEach(Console.WriteLine);
+
+        frutas.Clear();
     }
 }
