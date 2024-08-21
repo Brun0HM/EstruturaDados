a1c3c1d63e6710712a7d148f0b52505242e985c4
Brun0HM <brunodev41@gmail.com>
2024-08-21 10:57:49 -0300

    mudanca

--- EstruturaDeDados/Program.cs
+++ EstruturaDeDados/Program.cs
@@ -6,6 +6,8 @@
         Console.WriteLine("Working witht Git\\Github");
         Console.WriteLine("ablubleblelblelbelbleblelbe");
         Console.WriteLine("ablubleblelblelbelbleblelbe");
+        Console.WriteLine("ablubleblelblelbelbleblelbe");
+
 
 
     }
